cc9960f18048e7e9df67202ae88163ccc9b9077a
DK_Canyon <trgiangg29@gmail.com>
2026-09-12 10:25:38 +0700

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.Write("Nhập a");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b, c;
+
+        Console.Write("Nhap a = ");
+        a = int.Parse(Console.ReadLine());
+
+        Console.Write("Nhap b = ");
+        b = int.Parse(Console.ReadLine());
+
+        Console.Write("Nhap c = ");
+        c = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+        Console.WriteLine("c = " + c);
+    }
+}
